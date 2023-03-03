be0f60f09c9d70083a043cdd188fcc3b12ffbc9e
FelipeArnus <79066040+FelipeArnus@users.noreply.github.com>
2023-03-03 17:28:07 -0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,12 +1,17 @@
 # hamletuce
 
 
-print("  hello there", sep = '')# | print("fala interior") | o | sep = " " | pula a linha
+def new_func(x):#declara função
 
-x = int(input(""))#Sempre melhor fazer um input separado
-
-if x < 5: #if não precisa de parenteses a n ser que seja algo mais complexo como | if (x>5) and (x<6): |
+ if x < 5: #|if| não precisa de parenteses a n ser que seja algo mais complexo como |if (x>5) and (x<6):|
     print("  Perdeu")
 
-else:
-    print("  Ganhamo", x) #| , | obviamente cria uma sequencia
+ else:
+    print("  Ganhamo", x) # |,| obviamente cria uma sequencia
+
+    new_func(x)#termina função
+print("  hello there", sep = '')# |print("fala interior")| o |sep = " "| pula a linha
+
+x = int(input(""))# Sempre melhor fazer um input separado
+
+new_func(x)#ativa função
