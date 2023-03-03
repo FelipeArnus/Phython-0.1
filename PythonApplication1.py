c91aeca5875146ca01564f9fce4f0987e100df29
FelipeArnus <79066040+FelipeArnus@users.noreply.github.com>
2023-03-03 17:13:06 -0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,8 +1,12 @@
-# test
+# hamletuce
 
 
-x = int(input('  Oque há velhinho? '))
-if x == 5:
-    print('  We Go')
+print("  hello there", sep = '')# | print("fala interior") | o | sep = " " | pula a linha
+
+x = int(input(""))#Sempre melhor fazer um input separado
+
+if x < 5: #if nÃ£o precisa de parenteses a n ser que seja algo mais complexo como | if (x>5) and (x<6): |
+    print("  Perdeu")
+
 else:
-    print('  Nah')
+    print("  Ganhamo", x) #| , | obviamente cria uma sequencia
